14b9f7b163de48ba4be08650cdbebdb9702775b1
asergeev <asergeev.me@yandex.ru>
2022-12-15 11:45:25 +0300

    Completed Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите день недели: ");
+int weekday = Convert.ToInt32(Console.ReadLine());
+
+switch (weekday)
+{
+    case 1:
+        Console.WriteLine($"{weekday} -> Понедельник");
+        break;
+    case 2:
+        Console.WriteLine($"{weekday} -> Вторник");
+        break;
+    case 3:
+        Console.WriteLine($"{weekday} -> Среда");
+        break;
+    case 4:
+        Console.WriteLine($"{weekday} -> Четверг");
+        break;
+    case 5:
+        Console.WriteLine($"{weekday} -> Пятница");
+        break;
+    case 6:
+        Console.WriteLine($"{weekday} -> Суббота");
+        break;
+    case 7:
+        Console.WriteLine($"{weekday} -> Воскресенье");
+        break;
+    default:
+        Console.WriteLine($"{weekday} -> Таких дней недели нет");
+        break;
+}
