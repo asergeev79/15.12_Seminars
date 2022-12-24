5ff632202aa56ab05df73541cb26be69d3e013c7
Андрей Сергеев <asergeev@me.com>
2022-12-24 18:46:42 +0300

    Tak15 completed

---
+++ Task15/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 15: Напишите программу, 
+// которая принимает на вход цифру, 
+// обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+//
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите день недели: ");
+string weekday = Console.ReadLine();
+
+Console.WriteLine(Holiday(weekday));
+
+string Holiday(string day)
+{
+    switch (day)
+    {
+        case "1":
+            return $"{day} -> Понедельник не является выходным днём";
+            break;
+        case "2":
+            return $"{day} -> Вторник не является выходным днём";
+            break;
+        case "3":
+            return $"{day} -> Среда не является выходным днём";
+            break;
+        case "4":
+            return $"{day} -> Четверг не является выходным днём";
+            break;
+        case "5":
+            return $"{day} -> Пятница не является выходным днём";
+            break;
+        case "6":
+            return $"{day} -> Суббота является выходным днём";
+            break;
+        case "7":
+            return $"{day} -> Воскресенье является выходным днём";
+            break;
+        default:
+            return $"{day} -> Таких дней недели нет";
+            break;
+    }
+}
