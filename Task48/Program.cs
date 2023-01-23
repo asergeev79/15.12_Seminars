6030e97e2b5cda8d2354727de21776492e2f3718
Andrei Sergeev <asergeev.me@yandex.ru>
2023-01-23 11:38:16 +0300

    Tasks 46,48,49,51 completed

---
+++ Task48/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int countRows = EnterNumber("Введите кол-во строк массива: ");
+int countCols = EnterNumber("Введите кол-во столбцов массива: ");
+// int left = EnterNumber("Введите левую границу диапазона для случайных чисел: ");
+// int right = EnterNumber("Введите правую границу диапазона для случайных чисел: ");
+
+// if (!IsCorrectInput(left, right))
+// {
+//     Console.WriteLine("Левая граница должна быть меньше правой!");
+// }
+// else
+// {
+    int[,] array2D = CreateMatrix(countRows, countCols);
+    Console.WriteLine("В массиве");
+    PrintMatrix(array2D);
+// }
+
+
+int EnterNumber(string text) 
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// bool IsCorrectInput(int min, int max)
+// {
+//     return (min >= max) ? false : true;
+// }
+
+int[,] CreateMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+            matrix[i, j] = i + j;
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        Console.Write($"[{matrix[i, 0], 5}");
+        for (int j = 1; j < matrix.GetLength(1); j++) 
+            Console.Write($", {matrix[i, j], 5}");
+        Console.WriteLine(" ]");
+    }
+}
+
---
+++ Task49/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int countRows = EnterNumber("Введите кол-во строк массива: ");
+int countCols = EnterNumber("Введите кол-во столбцов массива: ");
+int left = EnterNumber("Введите левую границу диапазона для случайных чисел: ");
+int right = EnterNumber("Введите правую границу диапазона для случайных чисел: ");
+
+if (!IsCorrectInput(left, right))
+{
+    Console.WriteLine("Левая граница должна быть меньше правой!");
+}
+else
+{
+    int[,] array2D = CreateMatrix(countRows, countCols, left, right);
+    Console.WriteLine("Исходный массив:");
+    PrintMatrix(array2D);
+    MatrixEvenSquare(array2D);
+    Console.WriteLine("Новый массив:");
+    PrintMatrix(array2D);
+}
+
+
+int EnterNumber(string text) 
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool IsCorrectInput(int min, int max)
+{
+    return (min >= max) ? false : true;
+}
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(1); j++) 
+            matrix[i, j] = rnd.Next(min, max + 1);
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        Console.Write($"[{matrix[i, 0], 5}");
+        for (int j = 1; j < matrix.GetLength(1); j++) 
+            Console.Write($", {matrix[i, j], 5}");
+        Console.WriteLine(" ]");
+    }
+}
+
+void MatrixEvenSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+            matrix[i, j] *= matrix[i, j];
+}
